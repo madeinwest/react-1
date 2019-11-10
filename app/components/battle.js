321c06ef611797a7c66aac0e2cabf5c5b67d60e2
madeinwest <klymkovskyy@gmail.com>
2019-11-10 21:32:18 +0200

    inputs

--- app/components/battle.js
+++ app/components/battle.js
@@ -1,5 +1,6 @@
 var React = require('react');
-import {FaUserFriends, FaFighterJet, FaTrophy} from 'react-icons/fa';
+import {FaUserFriends, FaFighterJet, FaTrophy, FaTimesCircle} from 'react-icons/fa';
+var PropTypes = require('prop-types');
 
 function Instructions () {
 	return (
@@ -30,11 +31,157 @@ function Instructions () {
 		</div>
 	)
 }
+
+class PlayerInput extends React.Component {
+	constructor(props) {
+		super(props)
+
+		this.state ={
+			usermane: ''
+		}
+		this.handleSubmit = this.handleSubmit.bind(this);
+		this.handleChange = this.handleChange.bind(this);
+	}
+	handleSubmit (e) {
+		e.preventDefault();
+		this.props.onSubmit(this.state.usermane)
+	}
+	handleChange (e) {
+		this.setState({
+			usermane: e.target.value
+		})
+	}
+	render () {
+		return(
+			<form className="colums player" onSubmit={this.handleSubmit}>
+			<label htmlFor="username" className='player-label'>
+				{this.props.label}
+			</label>
+			<div className='row player-inputs'>
+				<input 
+				type='text'
+				id='username'
+				className='input-light'
+				placeholder='github-username'
+				autoComplete='off'
+				value = {this.state.username}
+				onChange={this.handleChange}
+				/>
+				<button
+					className='btn btn-dark'
+					type='submit'
+					disabled={!this.state.usermane}
+				>
+					Submit
+				</button>
+			</div>
+		</form>
+		)
+	}
+}
+
+PlayerInput.propTypes = {
+	onSubmit: PropTypes.func.isRequired,
+	label: PropTypes.string.isRequired
+}
+
+function PlayerPreview({username, onReset, label}) {
+	return(
+		<div className='column player'>
+			<h3 className="player-label">
+				{label}
+			</h3>
+			<div className="row bg-light">
+				<div className="player-info">
+					<img
+						className='avatar-small'
+						src={`https://github.com/${username}.png?size=200`}
+						alt={`Avatar for ${username}`}
+					/>
+					<a 
+						href={`https://github.com/${username}`}
+						className='link'
+						>
+						{username}
+					</a>
+				</div>
+				<button
+					className='btn-clear flex-center'
+					onClick={onReset}
+				>
+					<FaTimesCircle color='rgb(194, 57, 42)' size={26} />
+				</button>
+			</div>
+		</div>
+	)
+}
+PlayerPreview.proptypes ={
+	username: PropTypes.string.isRequired,
+	onReset: PropTypes.func.isRequired,
+	label: PropTypes.string.isRequired
+}
 export default class Battle extends React.Component{
+	constructor(props){
+		super(props)
+
+		this.state = {
+			playerOne:null,
+			playerTwo:null,
+		}
+
+		this.handleSubmit = this.handleSubmit.bind(this);
+		this.handleReset = this.handleReset.bind(this);
+
+	}
+	handleSubmit(id, player) {
+		this.setState({
+			[id]:player
+		})
+	}
+	handleReset(id){
+		this.setState({
+			[id]: null
+		})
+	}
 	render(){
+		const {playerOne, playerTwo} = this.state
 		return(
 			<React.Fragment>
 				<Instructions />
+
+				<div className='players-container'>
+					<h1 className='center-text header-lg'>
+						Players
+					</h1>
+					<div className="row space-around">
+						{playerOne === null
+							? (
+							<PlayerInput
+								label='Player One'
+								onSubmit={ (player) => this.handleSubmit('playerOne', player)}
+							/>
+						)
+							: <PlayerPreview
+									username={playerOne}
+									label='Player One'
+									onReset={() => this.handleReset('playerOne')}
+									/>
+								}
+						{playerTwo === null
+						 ? (
+							<PlayerInput
+								label='Player Two'
+								onSubmit={ (player) => this.handleSubmit('playerTwo', player)}
+							/>
+						)
+						: <PlayerPreview
+								username={playerTwo}
+								label='player Two'
+								onReset={() => this.handleReset('playerTwo')}
+							/>
+						}
+					</div>
+				</div>
 			</React.Fragment>
 		)
 	}
